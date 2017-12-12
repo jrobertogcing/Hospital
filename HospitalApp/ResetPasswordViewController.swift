b4c758e3d093796ab0fa399ecf70e5cdfe8c1fc2
Robert <Robert@macbook.local>
2017-12-11 21:32:23 -0600

    Reset  password ready.

--- HospitalApp/ResetPasswordViewController.swift
+++ HospitalApp/ResetPasswordViewController.swift
@@ -34,21 +34,43 @@ class ResetPasswordViewController: UIViewController {
 
     @IBAction func resetButtonAction(_ sender: UIButton) {
         
+        guard let userEmail = emailTextField.text, userEmail != "" else {
+            
+            // alert no data in text field
+            
+            alertGeneral(errorDescrip: "Fill all de fields", information: "Information")
+            
+            return
+        }
+
+        Auth.auth().sendPasswordReset(withEmail: userEmail) { error in
+            
+            if let error = error {
+            
+                self.alertGeneral(errorDescrip: error.localizedDescription, information: "Information")
+            
+            }else {
+            
+                self.alertSendEmailReset(email: userEmail)
+
+            
+            }//End if let error = error
+            
+        }//End Auth
+
         
-        
-        
-    }
+    }//End resetButtonAction
     
 //MARK: Alerts
     
     
     func alertSendEmailReset(email:String) {
         
-        let title = NSLocalizedString("reset.title", comment: "Ready!")
-        let message = NSLocalizedString("reset.message", comment: "Ready!")
+        //let title = NSLocalizedString("reset.title", comment: "Ready!")
+        //let message = NSLocalizedString("reset.message", comment: "Ready!")
         
         
-        let alertGeneral = UIAlertController(title: title, message: message + " " + email,  preferredStyle: .alert)
+        let alertGeneral = UIAlertController(title: title, message: "We have sent you an Email for reset your Password to:" + " " + email,  preferredStyle: .alert)
         
         let aceptAction = UIAlertAction(title: "Ok", style: .default)
         
